511f585fb2a5b0f17fa9807aa70efc413ac6b690
FernandoCM18 <fernando.c.m@live.com>
2023-04-26 14:34:43 -0700

    build: :mag: providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
 import './index.css';
+import { theme } from './styles/colorStyles';
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <ThemeProvider theme={theme.light}>
+        <App />
+      </ThemeProvider>
+    </HelmetProvider>
   </React.StrictMode>,
 );
